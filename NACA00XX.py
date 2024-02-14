f9d91d4665db6c363c136586a7276dedf51e3ea4
KhiZeus <cresus.gbado@gmail.com>
2024-02-13 23:51:05 -0500

    finalisation affichage

--- NACA00XX.py
+++ NACA00XX.py
@@ -11,7 +11,7 @@ def maximum_profil_naca00xx(epaisseur,corde,precision):
     yt = 5*epaisseur*(0.2969*xc**0.5-.1260*xc-.3516*xc**2+.2843*xc**3-.1036*xc**4)
     max_epaisseur = max(yt)
     max_position = np.argmax(yt)
-    return (xc[max_position],max_epaisseur)
+    return (corde*xc[max_position],max_epaisseur*corde)
 
 
 """ ----------------------------------------------------------------------------------------------------------
@@ -23,7 +23,7 @@ MAIN
 valeur_fournie_epaisseur= int(input("Donnez le numero (xx) de votre profil NACA symmetrique NACA00XX : "))
 corde = float(input("Quelle est la corde en mètre de votre profil NACA :"))
 distribution = input("Quelle distribution de point souhaitez vous avoir : lineaire (l) ou non-lineaire (n)")
-nombre_points = int(input("Combien de point voulez vous le long de la corde :"))
+nombre_points = int(input("Combien de points voulez vous le long de la corde :"))
 precision = 10**(-1-int(input("Quelle précision désirez vous pour l'épaisseur maximum :\n "
                       "\t\t\t\t1 : cm\n"
                       "\t\t\t\t2 : mm\n")))
@@ -47,15 +47,21 @@ ydown = -yt*corde
 x_reel= corde*xc
 Maximum = maximum_profil_naca00xx(epaisseur,corde,precision)
 print(precision,Maximum)
-Graph_naca = plt.figure(figsize=(8, 5))
+plt.figure(figsize=(10, 10*epaisseur+3))
 plt.plot(x_reel, yup, 'g--', label='Extrados')
 plt.plot(x_reel, ydown, 'r--', label='Intrados')
-#plt.text(0.5, 0.05, 'Point de fonctionnement')
+plt.axis([-.01*corde, 1.01*corde, -1.02*Maximum[1]-0.1, 1.02*Maximum[1]+0.1])
 plt.xlabel('position de corde (m)')
 plt.ylabel('épaisseur (m)')
 plt.title('Représentation graphique du profil NACA 00' + str(valeur_fournie_epaisseur))
 plt.legend()
-plt.annotate('Maximum', xy=Maximum,xytext=(0.5,0.0),arrowprops=dict(facecolor='blue',arrowstyle='<-'))
+bbox = dict(boxstyle="round", fc="0.8")
+arrowprops = dict(arrowstyle="->",connectionstyle="angle,angleA=0,angleB=90,rad=10")
+
+offset = 18
+plt.annotate('Maximum', xy=Maximum,
+             xytext=(-2*offset, offset), textcoords='offset points', bbox=bbox, arrowprops=arrowprops)
+
 plt.show()
 
 
